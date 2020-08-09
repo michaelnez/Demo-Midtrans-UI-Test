d108b689ed414ed2e0d78f967e5202991ffa7472
michaelnez <michael.ernes@gmail.com>
2020-08-09 17:28:50 +0700

    add assert shopping cart case

--- cypress/integration/Test/test-ui.js
+++ cypress/integration/Test/test-ui.js
@@ -4,13 +4,16 @@ describe('UI Test of demo.midtrans.com', function() {
     before(function(){
         cy.visit('https://demo.midtrans.com/')
     })
-    it('Verify URL is Correct', function(){
+    it('Assert URL is Correct', function(){
         cy.url().should('eq', 'https://demo.midtrans.com/')
     })
-    it(`Verify Product ${product_name} Appeared`, function() {
+    it(`Assert Product ${product_name} Appeared`, function() {
         cy.get('[data-reactid=".0.0.0.2.0.0.0"]').should('have.text', product_name)
     })
     it('Click "Buy Now" Button', function() {
-        cy.get('.buy')  .click()
+        cy.get('.buy').click()
+    })
+    it('Assert Product Added to Shopping Cart', function(){
+        cy.get('[data-reactid=".0.0.1.0.0.1"]').should('be.exist').and('have.text', "1")
     })
 })
